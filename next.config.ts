356c468abc38c1e61e1658589a78a78fb717d70f
codebyalexx <a.legal.contact@gmail.com>
2025-07-20 05:04:25 +0200

    :zap: Improved next config for remote urls

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      new URL(`${process.env.API_HOST}/**`),
+      new URL("https://img.coomer.su/**"),
+    ],
+  },
 };
 
 export default nextConfig;
